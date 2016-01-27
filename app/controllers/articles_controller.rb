f7f99ace79b48c56feec8ffab9c40f0620a6d2ea
Madhukar Tattikota <madhukart26@gmail.com>
2016-01-27 02:35:42 -0500

    added route to delete, views and custom messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -16,11 +22,34 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article is succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article is successfully deleted"
+    redirect_to articles_path
   end
 
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
